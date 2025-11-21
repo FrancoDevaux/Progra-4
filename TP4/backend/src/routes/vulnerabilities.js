2865371f3b0eb43d029ae50ed1564a4e84cffe69
PerroneEros <erosperr@gmail.com>
2025-11-21 18:06:28 -0300

    Resolucion Test 3

--- TP4/backend/src/routes/vulnerabilities.js
+++ TP4/backend/src/routes/vulnerabilities.js
@@ -2,17 +2,35 @@ const express = require('express');
 const router = express.Router();
 const vulnerabilityController = require('../controllers/vulnerabilityController');
 const { uploadMiddleware, uploadFile } = require('../controllers/uploadController');
+const csrf = require('csurf');
 
-// Command Injection
+const csrfProtection = csrf();
+
+const validateOrigin = (req, res, next) => {
+  const origin = req.get('origin');
+  const allowedOrigins = ['http://localhost:3000']; 
+  
+  if (origin && !allowedOrigins.includes(origin)) {
+    return res.status(403).json({ error: 'Origin invalido' });
+  }
+  next();
+};
+
 router.post('/ping', vulnerabilityController.ping);
 
-// CSRF - Transferencia
-router.post('/transfer', vulnerabilityController.transfer);
+router.get('/csrf-token', csrfProtection, vulnerabilityController.getCsrfToken);
 
-// Local File Inclusion
+router.post('/transfer', validateOrigin, csrfProtection, vulnerabilityController.transfer);
+
 router.get('/file', vulnerabilityController.readFile);
 
-// File Upload
 router.post('/upload', uploadMiddleware, uploadFile);
+
+router.use((err, req, res, next) => {
+  if (err.code === 'EBADCSRFTOKEN') {
+    return res.status(403).json({ error: 'CSRF token invalido o faltante' });
+  }
+  next(err);
+});
 
 module.exports = router;
