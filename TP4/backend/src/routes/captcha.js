cab39889cea0a787411bf0ddbc9e9db1ebdebaff
FrancoDeavux <166407581+FrancoDevaux@users.noreply.github.com>
2025-11-27 14:43:41 -0300

    finalizado

--- TP4/backend/src/routes/captcha.js
+++ TP4/backend/src/routes/captcha.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const captchaController = require('../controllers/captchaController');
 
-// Rutas requeridas por el test 6
+// Rutas de CAPTCHA
 router.get('/captcha', captchaController.generateCaptcha);
 router.post('/verify-captcha', captchaController.verifyCaptcha);
 
